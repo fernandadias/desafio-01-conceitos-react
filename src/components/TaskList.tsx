913cad54dd12fb09a5c61191ed0218964cec01d3
Fernanda Dias <fernandadias@8478-NB.local>
2021-06-01 20:11:34 -0300

    Finalizando o desafio

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,14 +16,41 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+
+    if(!!newTaskTitle) {
+
+      const newTask = {
+        id: Math.random(),
+        title: newTaskTitle,
+        isComplete: false
+      }
+      
+      setTasks([...tasks, newTask]);
+
+      setNewTaskTitle('')
+      
+    }
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+
+    const newCompleteTasks = tasks.map(task => task.id === id ? {
+      ...task,
+      isComplete : !task.isComplete
+    } : task);
+
+    setTasks(newCompleteTasks)
+    
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+
+    const newRemovedTasks = tasks.filter(task => task.id !== id)
+    setTasks(newRemovedTasks)
+
+    
   }
 
   return (
